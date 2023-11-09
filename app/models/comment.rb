1f6f8356261b37ea67b6ebb738c473f4cebd768c
Qaisar <kayser.abbas@gmail.com>
2023-11-09 14:47:33 +0500

    Remove extra spaces

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,9 +2,10 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
- after_save :update_post_comments_counter
+  after_save :update_post_comments_counter
 
   private
+
   def
   update_post_comments_counter
     post.update(comments_counter: post.comments.count)
