da8e1d17e021efa0a8c573e953dcb50ed3ee7569
Qaisar <kayser.abbas@gmail.com>
2023-11-09 14:46:50 +0500

    remove after_create and add after_save method

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,10 +2,11 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  after_create :update_post_comments_counter
+ after_save :update_post_comments_counter
 
-  # Method to update the comments counter for a post
-  def update_post_comments_counter
+  private
+  def
+  update_post_comments_counter
     post.update(comments_counter: post.comments.count)
   end
 end
