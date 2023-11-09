fb82f05b664ea74564b8b7f98f28d579acd24ed2
Qaisar <kayser.abbas@gmail.com>
2023-11-09 20:08:56 +0500

    Add validation: Text present

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
+  validates :text, presence: true
   after_save :increment_post_comments_counter
 
   def increment_post_comments_counter
