caaa3a563b22aa51496c19cc624dcccb866a0087
Qaisar <kayser.abbas@gmail.com>
2023-11-09 14:51:12 +0500

    UPdate function name

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,13 +3,12 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
-  after_save :update_posts_counter
+  after_save :update_user_posts_counter
   def recent_comments
     comments.order(created_at: :desc).limit(5)
   end
 
   private
-
   def update_user_posts_counter
     author.update_posts_counter
   end
