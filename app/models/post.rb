dcb99a364849738069b162d983adcd72fee44f52
Qaisar <kayser.abbas@gmail.com>
2023-11-09 20:06:21 +0500

    Add validation: Comments Counter must be an integer greater than or equal to zero.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,8 @@ class Post < ApplicationRecord
   has_many :likes, dependent: :destroy
  
   validates :title, presence: true , length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  
   
   after_save :increment_user_post_counter
 
