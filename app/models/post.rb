9f8230bdbe309444ed04bbbba9a1b70f11257f48
Qaisar <kayser.abbas@gmail.com>
2023-11-09 18:15:26 +0500

    Update the code method for pots.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,16 +1,15 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :author, class_name: 'User'
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
-  after_save :update_user_posts_counter
+  after_create :increment_user_post_counter
+
   def recent_comments
     comments.order(created_at: :desc).limit(5)
   end
 
-  private
-
-  def update_user_posts_counter
-    author.update_posts_counter
+  def increment_user_post_counter
+    author.increment!(:post_counter)
   end
 end
