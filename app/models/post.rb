c60d1f1c64d53788f9ecdb9854776713c3bb34ca
Qaisar <kayser.abbas@gmail.com>
2023-11-09 20:04:48 +0500

    Add validation:  Title must not exceed 250 characters.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,8 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
-  
-  validates :title, presence: true
+ 
+  validates :title, presence: true , length: { maximum: 250 }
   
   after_save :increment_user_post_counter
 
