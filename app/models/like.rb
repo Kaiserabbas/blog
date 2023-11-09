14f0522e314091bffaddeb66d8e0aa77b99ce4a4
Qaisar <kayser.abbas@gmail.com>
2023-11-09 14:46:41 +0500

    remove after_create and add after_save method

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,10 +2,11 @@ class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  after_create :update_post_likes_counter
+  after_save :update_post_likes_counter
 
-  # Method to update the likes counter for a post
-  def update_post_likes_counter
+  private
+  def
+    update_post_likes_counter
     post.update(likes_counter: post.likes.count)
   end
 end
