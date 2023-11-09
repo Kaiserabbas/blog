fb6b2cb5800c5ababb1684ed6665998ef1967b3d
Qaisar <kayser.abbas@gmail.com>
2023-11-09 20:01:20 +0500

    Add validation:  Name must not be blank.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id', dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
-
+  validates :name, presence: true
   def recent_posts
     posts.order(created_at: :desc).limit(3)
   end
