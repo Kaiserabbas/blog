44efade7009ee86b6e0a7f9a4e7161afd16f4c2f
Qaisar <kayser.abbas@gmail.com>
2023-11-09 10:14:13 +0500

    Add foreign_key and Update after_create method

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,15 +1,15 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User'
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
-  after_create :increment_user_post_counter
-
+  after_create :update_user_posts_counter
   def recent_comments
     comments.order(created_at: :desc).limit(5)
   end
 
-  def increment_user_post_counter
-    author.increment!(:post_counter)
+  # Method to update the posts counter for a user
+  def update_user_posts_counter
+    author.update(posts_counter: author.posts.count)
   end
 end
