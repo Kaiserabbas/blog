8ba618299c230ce68c32927496402b4873803c77
Qaisar <kayser.abbas@gmail.com>
2023-11-09 18:15:17 +0500

    Update the code method for like.rb

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,12 +2,9 @@ class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  after_save :update_post_likes_counter
+  after_create :increment_post_likes_counter
 
-  private
-
-  def
-    update_post_likes_counter
-    post.update(likes_counter: post.likes.count)
+  def increment_post_likes_counter
+    post.increment!(:likes_counter)
   end
 end
