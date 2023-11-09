56aad90c6daee818cf0278b6ae4b4f6bc4fe147e
Qaisar <kayser.abbas@gmail.com>
2023-11-09 12:42:52 +0500

    Updated user.rb for update user posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,11 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id', dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
-
   def recent_posts
     posts.order(created_at: :desc).limit(3)
+  end
+
+  def update_user_posts_counter
+    update(posts_counter: posts.count)
   end
 end
