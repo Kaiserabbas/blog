da945e1e15a596c3f51c6bcd7fe622275a6e90a1
Qaisar <kayser.abbas@gmail.com>
2023-11-09 18:33:59 +0500

    Modify after_create to after_save

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  after_create :increment_post_comments_counter
+  after_save :increment_post_comments_counter
 
   def increment_post_comments_counter
     post.increment!(:comments_counter)
