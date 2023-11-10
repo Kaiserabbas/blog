c22ebb26979c050ebced31590bee1d8b637bc077
Qaisar <kayser.abbas@gmail.com>
2023-11-10 11:00:27 +0500

    Fix Rubocop errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id', dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
-  
+
   validates :name, presence: true
   validates :post_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   def recent_posts
