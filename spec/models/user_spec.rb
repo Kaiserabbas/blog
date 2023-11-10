ea2ecf7fc25b2227488ae36772fe96cf6b661156
Qaisar <kayser.abbas@gmail.com>
2023-11-10 15:07:59 +0500

    Add new tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,6 +18,18 @@ RSpec.describe User, type: :model do
     expect(user.errors[:post_counter]).to include('must be greater than or equal to 0')
   end
 
+  it 'is not valid with a non-integer posts_counter' do
+    user = User.new(name: 'Tom', post_counter: 1.5)
+    user.valid?
+    expect(user.errors[:post_counter]).to include('must be an integer')
+  end
+
+  it 'is not valid with a non-numeric posts_counter' do
+    user = User.new(name: 'Tom', post_counter: 'abc')
+    user.valid?
+    expect(user.errors[:post_counter]).to include('is not a number')
+  end
+
   it 'returns the 3 most recent posts for a user' do
     user = User.create(name: 'Tom', post_counter: 0)
     4.times { user.posts.create(title: 'My Post', comments_counter: 0, likes_counter: 0) }
