19a6bfdf55f698318c5d4fe12970bb6005bc2407
Qaisar <kayser.abbas@gmail.com>
2023-11-13 13:57:27 +0500

    Wrap posts' show route  within the users route

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   resources :users, only: [:index, :show] do
-      resources :posts, only: [:index]
+      resources :posts, only: [:index, :show]
   end
-  resources :posts, only: [:index, :show]
   root "posts#index"
 end
