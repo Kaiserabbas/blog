b7f22d25ed15f8bdf0602a8ee35f7e4aa94691b2
Qaisar <kayser.abbas@gmail.com>
2023-11-15 11:00:21 +0500

    Upfate for gem "Faker"

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# db/seeds.rb
+require 'faker'
+
+# Create the initial users
+first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
+
+# Create 8 more users with Faker
+8.times do
+  User.create(
+    name: Faker::Name.name,
+    photo: Faker::Internet.url('example.com'),
+    bio: Faker::Lorem.sentence
+  )
+end
