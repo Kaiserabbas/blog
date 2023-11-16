e98ceaf154c4326ef298fd4e70d5e86539e17e69
Qaisar <kayser.abbas@gmail.com>
2023-11-16 12:54:24 +0500

    Update def 'new' and def 'create'

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,26 +1,47 @@
 class PostsController < ApplicationController
   before_action :set_user
   before_action :set_post, only: [:show]
+  before_action :initialize_like
+
+  def new
+    @post = @user.posts.new
+  end
+  def create
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      redirect_to user_posts_path(current_user), notice: 'Post was successfully created.'
+    else
+      render :new
+    end
+  end
 
   def index
-    @posts = @user.posts
+    @user = User.find(params[:user_id])
+    @posts = Post.where(author_id: params[:user_id])
   end
 
   def show
+    @user = User.find_by_id(params[:user_id])
+    @post = Post.find_by_id(params[:id])
+    @like = current_user.likes.find_by(post: @post) || Like.new
     @comments = @post.comments
   end
 
   private
 
   def set_user
-    @user = User.find(params[:user_id])
+    @user = current_user
   end
 
   def set_post
-    @post = @user.posts.find(params[:id])
+    @post = current_user.posts.find(params[:id])
   end
 
+  def initialize_like
+    @like = Like.new
+  end
   def post_params
-    params.require(:post).permit(:title, :content, :author_id)
+    params.require(:post).permit(:title, :text)
   end
 end
