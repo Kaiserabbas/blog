f0358248bb49db9aac0e5d2771691a995d3fe7b7
Qaisar <kayser.abbas@gmail.com>
2023-11-16 12:57:49 +0500

    Fix rubocop linters errors

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,40 +1,43 @@
 class LikesController < ApplicationController
-  before_action :find_post, only: [:new, :create]
+  before_action :find_post, only: %i[new create]
   before_action :set_user
   before_action :set_post
   before_action :set_like, only: [:destroy]
 
-    def new
-        @like = @post.likes.new
-    end
+  def new
+    @like = @post.likes.new
+  end
 
-    def create
+  def create
     @like = @post.likes.new(user: current_user)
 
     if @like.save
-        redirect_to user_post_path(@post.author, @post), notice: 'Post liked!'
+      redirect_to user_post_path(@post.author, @post), notice: 'Post liked!'
     else
-        render :new
+      render :new
     end
-    end
+  end
 
-    def destroy
-        @like.destroy
-        redirect_to user_post_path(@post.author, @post)
-    end
+  def destroy
+    @like.destroy
+    redirect_to user_post_path(@post.author, @post)
+  end
 
-    private
+  private
 
-    def set_like
-        @like = @post.likes.find(params[:id])
-    end       
-    def set_user
-        @user = User.find(params[:user_id])
-    end 
-    def set_post
-        @post = @user.posts.find(params[:post_id])
-    end
-    def find_post
-        @post = Post.find(params[:post_id])
-    end
+  def set_like
+    @like = @post.likes.find(params[:id])
+  end
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def set_post
+    @post = @user.posts.find(params[:post_id])
+  end
+
+  def find_post
+    @post = Post.find(params[:post_id])
+  end
 end
