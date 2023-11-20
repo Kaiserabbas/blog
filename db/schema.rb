e15b4646b5e47a5dabfd43a3e43a4ffd9c7d8329
Kaiserabbas <123939543+Kaiserabbas@users.noreply.github.com>
2023-11-20 16:26:34 +0500

    Add posts_count to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_20_111458) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_20_112450) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,6 +51,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_20_111458) do
     t.integer "post_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "posts_count", default: 0
   end
 
   add_foreign_key "comments", "posts"
