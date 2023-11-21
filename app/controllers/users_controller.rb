7c172ec35b834b2dde8ea0a61153db2b1a33d493
Qaisar <kayser.abbas@gmail.com>
2023-11-21 14:20:34 +0500

    Update UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    @users = User.includes(:posts).all
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = User.includes(posts: :author).find(params[:id])
     @posts = @user.posts
   end
 end
--- db/migrate/20231120111458_change_post_counter_default_in_users.rb
+++
@@ -1,5 +0,0 @@
-class ChangePostCounterDefaultInUsers < ActiveRecord::Migration[7.1]
-  def change
-        change_column_default :users, :post_counter, from: nil, to: 0
-  end
-end
