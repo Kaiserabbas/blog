308dc0a9cd8645e2c301dba335a15688c9178de8
Qaisar <kayser.abbas@gmail.com>
2023-11-23 12:22:22 +0500

    Update the schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_20_112450) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_22_095519) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,10 +48,9 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_20_112450) do
     t.string "name"
     t.string "photo"
     t.text "bio"
-    t.integer "post_counter", default: 0
+    t.integer "posts_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "posts_count", default: 0
   end
 
   add_foreign_key "comments", "posts"
