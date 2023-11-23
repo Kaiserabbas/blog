23ff0577a59c8f8f77ec5ad31dd362e6270495b5
Qaisar <kayser.abbas@gmail.com>
2023-11-23 12:20:14 +0500

    Update the spec file

--- spec/features/user_show_spec.rb
+++ spec/features/user_show_spec.rb
@@ -1,25 +1,26 @@
 require 'rails_helper'
 
 RSpec.feature 'User show page' do
-  let!(:user1) { User.create(name: 'Qaisar', bio: 'A student from Morocco', photo: 'https://avatars.githubusercontent.com/u/123939543?v=4') }
-  let!(:post1) { Post.create(author: user1, title: 'Post 01') }
-  let!(:post2) { Post.create(author: user1, title: 'Post 02') }
-  let!(:post3) { Post.create(author: user1, title: 'Post 03') }
-  let!(:post4) { Post.create(author: user1, title: 'Post 04') }
+  let!(:user1) { User.create(name: 'Qaisar', bio: 'A student from Pakistan', photo: 'https://avatars.githubusercontent.com/u/123939543?v=4') }
+  let!(:post1) { Post.create(author: user1, title: 'Post 01', text: 'Hello World!') }
+  let!(:post2) { Post.create(author: user1, title: 'Post 02', text: 'Hello World!') }
+  let!(:post3) { Post.create(author: user1, title: 'Post 03', text: 'Hello World!' ) }
+  let!(:post4) { Post.create(author: user1, title: 'Post 04', text: 'Hello World!' ) }
 
   scenario 'Displays user Profile picture, username, bio and number of posts' do
     visit user_path(user1)
     expect(page).to have_css("img[src='#{user1.photo}']")
     expect(page).to have_content(user1.name)
     expect(page).to have_content(user1.bio)
-    expect(page).to have_content("Number of posts: #{user1.posts_counter}")
+    expect(page).to have_content("No of Posts: #{user1.posts.count}")
+    save_and_open_page
   end
 
   scenario 'Displays user 3 posts' do
     visit user_path(user1)
-
-    expect(page).to have_content(post1.title)
+    expect(page).to have_content(post2.title)
     expect(page).to have_content(post3.title)
+    expect(page).to have_content(post4.title)
   end
 
   scenario 'Displays Button that lets me view all of a user posts, and redirects to the posts when clicked' do
@@ -33,8 +34,16 @@ RSpec.feature 'User show page' do
   scenario 'click on user post redirects to that post show page.' do
     visit user_path(user1)
 
-    click_link post1.title
+    click_link post2.title
     sleep(1)
-    expect(current_path).to eq(user_post_path(user1, post1))
+    expect(current_path).to eq(user_post_path(user1, post2))
   end
+  
+  scenario 'Click to see all posts, it redirects me to the users posts index page.' do
+    visit user_path(user1)
+    
+    click_link 'See all posts'
+    sleep(1)
+    expect(current_path).to eq(user_posts_path(user1))
+  end      
 end
