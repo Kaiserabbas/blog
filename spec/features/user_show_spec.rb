737ebba938618b98565638383ce0fd92f3565d17
Qaisar <kayser.abbas@gmail.com>
2023-11-23 12:33:09 +0500

    Fix rubocop linters errors

--- spec/features/user_show_spec.rb
+++ spec/features/user_show_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.feature 'User show page' do
-  let!(:user1) { User.create(name: 'Qaisar', bio: 'A student from Pakistan', photo: 'https://avatars.githubusercontent.com/u/123939543?v=4') }
+  let!(:user1) { User.create(name: 'Qaisar', bio: 'A student', photo: 'https://avatars.githubusercontent.com/u/123939543?v=4') }
   let!(:post1) { Post.create(author: user1, title: 'Post 01', text: 'Hello World!') }
   let!(:post2) { Post.create(author: user1, title: 'Post 02', text: 'Hello World!') }
-  let!(:post3) { Post.create(author: user1, title: 'Post 03', text: 'Hello World!' ) }
-  let!(:post4) { Post.create(author: user1, title: 'Post 04', text: 'Hello World!' ) }
+  let!(:post3) { Post.create(author: user1, title: 'Post 03', text: 'Hello World!') }
+  let!(:post4) { Post.create(author: user1, title: 'Post 04', text: 'Hello World!') }
 
   scenario 'Displays user Profile picture, username, bio and number of posts' do
     visit user_path(user1)
@@ -13,7 +13,6 @@ RSpec.feature 'User show page' do
     expect(page).to have_content(user1.name)
     expect(page).to have_content(user1.bio)
     expect(page).to have_content("No of Posts: #{user1.posts.count}")
-    save_and_open_page
   end
 
   scenario 'Displays user 3 posts' do
@@ -38,12 +37,12 @@ RSpec.feature 'User show page' do
     sleep(1)
     expect(current_path).to eq(user_post_path(user1, post2))
   end
-  
+
   scenario 'Click to see all posts, it redirects me to the users posts index page.' do
     visit user_path(user1)
-    
+
     click_link 'See all posts'
     sleep(1)
     expect(current_path).to eq(user_posts_path(user1))
-  end      
+  end
 end
