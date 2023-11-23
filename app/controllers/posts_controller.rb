7fb0b2102afa14d2437b742fbd761de23aadc508
Qaisar <kayser.abbas@gmail.com>
2023-11-23 12:06:27 +0500

    Update the controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,40 +1,36 @@
 class PostsController < ApplicationController
-  before_action :set_user
   before_action :set_post, only: [:show]
   before_action :initialize_like
 
   def new
-    @post = @user.posts.new
+    @user = User.find(params[:user_id])
+    @post = Post.build
   end
 
   def create
-    @post = current_user.posts.build(post_params)
+    @user = User.find(params[:user_id])
+    @post = @user.posts.build(post_params)
 
     if @post.save
-      redirect_to user_posts_path(current_user), notice: 'Post was successfully created.'
+      redirect_to user_post_path(@user, @post), notice: 'Post was successfully created.'
     else
-      logger.error @post.errors.inspect
       render :new
     end
   end
 
   def index
     @user = User.find(params[:user_id])
-    @posts = @user.posts.includes(:comments, :author, :likes)
+    @posts = @user.posts.includes(:comments).paginate(page: params[:page], per_page: 6)
   end
 
   def show
     @user = User.find_by_id(params[:user_id])
     @post = Post.find_by_id(params[:id])
-    @like = current_user.likes.find_by(post: @post) || Like.new
+    @like = @post.likes
     @comments = @post.comments
   end
 
   private
-
-  def set_user
-    @user = current_user
-  end
 
   def set_post
     @post = Post.find(params[:id])
