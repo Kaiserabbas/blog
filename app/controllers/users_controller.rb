96875540f07a4ccf624fb6a907322b205405508b
Qaisar <kayser.abbas@gmail.com>
2023-11-24 19:00:00 +0500

    Update User Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @users = User.includes(:posts).all
+    @users = User.includes(:posts).paginate(page: params[:page], per_page: 6)
   end
 
   def show
