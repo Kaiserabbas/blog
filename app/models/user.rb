f86eb5a3ad04fe0ce038b6a1c5e473b445244804
Qaisar <kayser.abbas@gmail.com>
2023-11-24 19:00:15 +0500

    Update User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,9 +14,9 @@ class User < ApplicationRecord
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   attribute :posts_counter, :integer, default: 0
   
-    def admin?
+  def admin?
       role == 'admin'
-    end
+  end
 
   def recent_posts
     posts.order(created_at: :desc).limit(3)
