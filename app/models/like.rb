6fd8771020f45f9e5bd0218f319acef070aed7dc
Kaiserabbas <123939543+Kaiserabbas@users.noreply.github.com>
2023-11-24 09:45:51 +0500

    Update the model

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,6 +3,7 @@ class Like < ApplicationRecord
   belongs_to :post
 
   after_save :increment_post_likes_counter
+  validates :user, uniqueness: { scope: :post }
 
   def increment_post_likes_counter
     post.increment!(:likes_counter)
