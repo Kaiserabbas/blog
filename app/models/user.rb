9a42820a7e2db6895d268eec767b69219f309977
Qaisar <kayser.abbas@gmail.com>
2023-11-24 12:34:59 +0500

    Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,15 +8,15 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
 
   attr_accessor :unconfirmed_email
-
-  def admin?
-    role == 'admin'
-  end
-
+  
   validates :email, presence: true
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   attribute :posts_counter, :integer, default: 0
+  
+    def admin?
+      role == 'admin'
+    end
 
   def recent_posts
     posts.order(created_at: :desc).limit(3)
