4b7893dfb3155b04c57df47b461cb77af4c000ab
Qaisar <kayser.abbas@gmail.com>
2023-11-24 18:58:48 +0500

    Update the Comment Controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,63 +1,59 @@
-class CommentsController < ApplicationController
-  before_action :set_user
-  before_action :set_post
-  before_action :set_comment, only: [:show]
-
-  def new
-    @comment = @post.comments.new
-  end
-
-  def create
-    @comment = current_user.comments.build(comment_params.merge(post: @post))
-    if @comment.save
-      redirect_to user_post_path(@post.author, @post)
-    else
-      render :new
+  class CommentsController < ApplicationController
+    load_and_authorize_resource
+    before_action :set_post
+    def new
+      @comment = Comment.new
     end
-  end
 
-  def edit
-    @comment = @post.comments.find(params[:id])
-  end
-
-  def update
-    @comment = @post.comments.find(params[:id])
-    if @comment.update(comment_params)
-      redirect_to user_post_path(@post.author, @post), notice: 'Comment updated successfully'
-    else
-      render :edit
+    def create
+      @comment = current_user.comments.build(comment_params.merge(post: @post))
+      if @comment.save
+        @post.comments_counter += 1
+        redirect_to user_post_path(@post.author, @post)
+      else
+        render :new
+      end
     end
-  end
 
-  def show
-    @comments = @post.comments
-  end
+    def edit
+      @comment = @post.comments.find(params[:id])
+    end
+
+    def update
+      @comment = @post.comments.find(params[:id])
+      if @comment.update(comment_params)
+        redirect_to user_post_path(@post.author, @post), notice: 'Comment updated successfully'
+      else
+        render :edit
+      end
+    end
+
+    def show
+      @comments = @post.comments
+    end
 
     def destroy
-    @comment = Comment.find(params[:id])
-    @post = @comment.post
-    @user = @post.author
-    @comment.destroy
-    @post.comments_counter -= 1
+      @comment = Comment.find(params[:id])
+      @post = @comment.post
 
-    redirect_to user_post_path(@user, @post) if @post.save
+      if @comment.destroy
+        @post.update(comments_counter: @post.comments.count)
+        flash[:notice] = 'Comment deleted successfully.'
+      else
+        flash[:error] = 'Failed to delete comment.'
+      end
+
+      redirect_to user_post_path(@post.author, @post)
+    end
+    
+
+    private
+
+    def set_post
+      @post = Post.find(params[:post_id])
+    end
+
+    def comment_params
+      params.require(:comment).permit(:text)
+    end
   end
-
-  private
-
-  def set_user
-    @user = User.find(params[:user_id])
-  end
-
-  def set_post
-    @post = @user.posts.find(params[:post_id])
-  end
-
-  def set_comment
-    @comment = @post.comments.find(params[:id])
-  end
-
-  def comment_params
-    params.require(:comment).permit(:text)
-  end
-end
