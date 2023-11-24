d0955597b1af9d4c444e4540d2cd19c8b04d5be5
Kaiserabbas <123939543+Kaiserabbas@users.noreply.github.com>
2023-11-24 09:44:26 +0500

    Update user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,12 @@ RSpec.describe User, type: :model do
     expect(user).to be_valid
   end
 
+  it 'is invalid without an email' do
+    user = User.new(email: nil)
+    user.valid?
+    expect(user.errors[:email]).to include("can't be blank")
+  end
+
   it 'is not valid without a name' do
     user = User.new(name: nil)
     user.valid?
